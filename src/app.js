394e1606a8838b553f9712693b62ec81fa27bb5b
Juan Carreño <jcarrenoar@unal.edu.co>
2025-01-29 00:31:27 -0500

    update swagger

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const options = {
         },
         servers:[
             {
-                url: 'http://localhost:3001/api/',
+                url: `${process.env.SERVER_PRODUCTION}api/`,
             }
         ]
     },
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -5,20 +5,73 @@ const apiController = require('../controllers/apiController');
 
 /**
  * @swagger
- * /users/{id}:
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         uid:
+ *           type: string
+ *           description: ID único del usuario.
+ *           example: "123456"
+ *         nombres:
+ *           type: string
+ *           description: Nombre completo del usuario.
+ *           example: "Juan Pérez"
+ *         email:
+ *           type: string
+ *           description: Correo electrónico del usuario.
+ *           example: "juan.perez@example.com"
+ *         url_imagen:
+ *           type: string
+ *           description: URL de la imagen de perfil del usuario.
+ *           example: ""
+ *         fecha_nacimiento:
+ *           type: object
+ *           properties:
+ *             seconds:
+ *               type: integer
+ *               example: 1738042253
+ *             nanoseconds:
+ *               type: integer
+ *               example: 117000000
+ *         fecha_registro:
+ *           type: string
+ *           format: date-time
+ *           description: Fecha de registro del usuario.
+ *           example: "2025-01-15T12:00:00.000Z"
+ *         rol:
+ *           type: string
+ *           description: Rol del usuario.
+ *           example: "usuario"
+ *         ajustes:
+ *           type: object
+ *           properties:
+ *             notificaciones:
+ *               type: boolean
+ *               example: true
+ *             tema:
+ *               type: string
+ *               example: "oscuro"
+ */
+
+/**
+ * @swagger
+ * /users/{uid}:
  *   get:
- *     summary: Consultar usuario por ID
- *     description: Obtiene los datos de un usuario específico según su ID.
+ *     summary: Obtiene un usuario por UID.
+ *     tags:
+ *       - Usuarios
  *     parameters:
  *       - in: path
- *         name: id
+ *         name: uid
  *         required: true
  *         schema:
  *           type: string
- *         description: ID del usuario.
+ *         description: UID del usuario a buscar.
  *     responses:
  *       200:
- *         description: Usuario obtenido correctamente.
+ *         description: Usuario encontrado.
  *         content:
  *           application/json:
  *             schema:
@@ -35,7 +88,7 @@ const apiController = require('../controllers/apiController');
  *                       example: "123456"
  *                     nombres:
  *                       type: string
- *                       example: "Juan Pérez"
+ *                       example: "Juan Andres C"
  *                     email:
  *                       type: string
  *                       example: "juan.perez@example.com"
@@ -44,16 +97,18 @@ const apiController = require('../controllers/apiController');
  *                       example: ""
  *                     fecha_nacimiento:
  *                       type: object
+ *                       description: Fecha de nacimiento en formato TimeStamp de Firebase.
  *                       properties:
- *                         _seconds:
+ *                         seconds:
  *                           type: integer
- *                           example: 1738042253
- *                         _nanoseconds:
+ *                           example: 1738123242
+ *                         nanoseconds:
  *                           type: integer
- *                           example: 117000000
+ *                           example: 927000000
  *                     fecha_registro:
  *                       type: string
  *                       format: date-time
+ *                       description: Fecha de registro en formato ISO 8601.
  *                       example: "2025-01-15T12:00:00.000Z"
  *                     rol:
  *                       type: string
@@ -67,11 +122,19 @@ const apiController = require('../controllers/apiController');
  *                         tema:
  *                           type: string
  *                           example: "oscuro"
- *                     store:
- *                       type: array
- *                       items:
- *                         type: string
- *                       example: []
+ *       404:
+ *         description: Usuario no encontrado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Error"
  */
 router.get('/users/:id', apiController.getUser); // Consultar usuario por ID
 
@@ -79,8 +142,9 @@ router.get('/users/:id', apiController.getUser); // Consultar usuario por ID
  * @swagger
  * /users:
  *   post:
- *     summary: Crear un usuario
- *     description: Crea un nuevo usuario en el sistema.
+ *     summary: Crea un nuevo usuario.
+ *     tags:
+ *       - Usuarios
  *     requestBody:
  *       required: true
  *       content:
@@ -88,41 +152,95 @@ router.get('/users/:id', apiController.getUser); // Consultar usuario por ID
  *           schema:
  *             type: object
  *             properties:
+ *               uid:
+ *                 type: string
+ *                 description: Identificador único del usuario.
+ *                 example: "123456"
  *               nombres:
  *                 type: string
- *                 description: Nombre completo del usuario.
+ *                 description: Nombres del usuario.
+ *                 example: "Juan Andres C"
  *               email:
  *                 type: string
  *                 description: Correo electrónico del usuario.
- *               imagen:
+ *                 example: "juan.perez@example.com"
+ *               url_imagen:
  *                 type: string
  *                 description: URL de la imagen del usuario.
+ *                 example: ""
  *               fecha_nacimiento:
+ *                 type: object
+ *                 description: Fecha de nacimiento en formato TimeStamp de Firebase.
+ *                 properties:
+ *                   seconds:
+ *                     type: integer
+ *                     example: 1738123242
+ *                   nanoseconds:
+ *                     type: integer
+ *                     example: 927000000
+ *               fecha_registro:
  *                 type: string
- *                 format: date
- *                 description: Fecha de nacimiento del usuario.
+ *                 format: date-time
+ *                 description: Fecha de registro en formato ISO 8601.
+ *                 example: "2025-01-15T12:00:00.000Z"
  *               rol:
  *                 type: string
  *                 description: Rol del usuario.
+ *                 example: "usuario"
+ *               ajustes:
+ *                 type: object
+ *                 description: Configuraciones del usuario.
+ *                 properties:
+ *                   notificaciones:
+ *                     type: boolean
+ *                     example: true
+ *                   tema:
+ *                     type: string
+ *                     example: "oscuro"
  *     responses:
  *       201:
- *         description: Usuario creado correctamente.
+ *         description: Usuario creado con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "Usuario creado con éxito."
+ *       400:
+ *         description: Error al crear el usuario.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Error al crear el usuario."
  */
 router.post('/users', apiController.createUser); // Crear un usuario
 
 /**
  * @swagger
- * /users/{id}:
+ * /users/{uid}:
  *   put:
- *     summary: Editar usuario por ID
- *     description: Actualiza los datos de un usuario existente.
+ *     summary: Actualiza un usuario existente.
+ *     tags:
+ *       - Usuarios
  *     parameters:
  *       - in: path
- *         name: id
+ *         name: uid
  *         required: true
  *         schema:
  *           type: string
- *         description: ID del usuario
+ *         description: UID del usuario a actualizar.
  *     requestBody:
  *       required: true
  *       content:
@@ -132,35 +250,94 @@ router.post('/users', apiController.createUser); // Crear un usuario
  *             properties:
  *               nombres:
  *                 type: string
- *                 description: Nombre completo del usuario.
+ *                 description: Nombres del usuario.
+ *                 example: "Juan Andres C"
  *               email:
  *                 type: string
  *                 description: Correo electrónico del usuario.
- *               rol:
+ *                 example: "juan.perez@example.com"
+ *               url_imagen:
  *                 type: string
- *                 description: Rol del usuario.
+ *                 description: URL de la imagen del usuario.
+ *                 example: ""
+ *               fecha_nacimiento:
+ *                 type: object
+ *                 description: Fecha de nacimiento en formato TimeStamp de Firebase.
+ *                 properties:
+ *                   _seconds:
+ *                     type: integer
+ *                     example: 1738123242
+ *                   nanoseconds:
+ *                     type: integer
+ *                     example: 927000000
+ *               ajustes:
+ *                 type: object
+ *                 description: Configuraciones del usuario.
+ *                 properties:
+ *                   notificaciones:
+ *                     type: boolean
+ *                     example: true
+ *                   tema:
+ *                     type: string
+ *                     example: "oscuro"
  *     responses:
  *       200:
- *         description: Usuario actualizado correctamente.
+ *         description: Usuario actualizado con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: "User updated successfully"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     success:
+ *                       type: boolean
+ *                       example: true
+ *                     message:
+ *                       type: string
+ *                       example: "Document updated successfully"
+ *       400:
+ *         description: Error al actualizar el usuario.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Error al actualizar el usuario."
  */
 router.put('/users/:id', apiController.updateUser); // Editar usuario por ID
 
 /**
  * @swagger
- * /users/{id}:
+ * /users/{uid}:
  *   delete:
- *     summary: Eliminar usuario por ID
- *     description: Elimina un usuario específico según su ID.
+ *     summary: Elimina un usuario por UID.
+ *     tags:
+ *       - Usuarios
  *     parameters:
  *       - in: path
- *         name: id
+ *         name: uid
  *         required: true
  *         schema:
  *           type: string
- *         description: ID del usuario
+ *         description: UID del usuario a eliminar.
  *     responses:
  *       200:
- *         description: Usuario eliminado correctamente.
+ *         description: Usuario eliminado con éxito.
+ *       404:
+ *         description: Usuario no encontrado.
  */
 router.delete('/users/:id', apiController.deleteUser); // Eliminar usuario por ID
 
@@ -168,26 +345,24 @@ router.delete('/users/:id', apiController.deleteUser); // Eliminar usuario por I
  * @swagger
  * /users/check/{id}:
  *   get:
- *     summary: Verificar si el usuario está registrado
- *     description: Comprueba si un usuario con un ID específico está registrado.
+ *     summary: Verifica si un usuario existe por id.
+ *     tags:
+ *       - Usuarios
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
  *         schema:
  *           type: string
- *         description: ID del usuario
+ *         description: UID del usuario.
  *     responses:
  *       200:
- *         description: Usuario verificado.
+ *         description: Resultado de la verificación de existencia del usuario.
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 registrado:
- *                   type: boolean
- *                   description: Indica si el usuario está registrado.
+ *               type: boolean
+ *               example: true
  */
 router.get('/users/check/:id', apiController.isUserRegistered); // Verificar si el usuario está registrado
 
