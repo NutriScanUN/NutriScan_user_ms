2fdf4d4d2247182ef6d29d99ac7087eb9f9c803f
Juan Carreño <jcarrenoar@unal.edu.co>
2025-03-02 14:08:32 -0500

    add prometheus route /metrics

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/apiRoutes');
 const SwaggerJSDoc = require('swagger-jsdoc');
 const SwaggerUI = require('swagger-ui-express');
+const promClient = require("prom-client");
+
 
 // Crear una instancia de la aplicación Express
 const app = express();
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
 
 // Configurar el middleware para parsear el cuerpo de las solicitudes (JSON)
 app.use(bodyParser.json());
@@ -33,6 +38,11 @@ app.use('/docs', SwaggerUI.serve,SwaggerUI.setup(swaggerSpec))
 
 // Rutas de la API
 app.use('/api', apiRoutes);  // Rutas para interactuar con el otro microservicio
+
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+  });
 
 // Ruta de bienvenida o predeterminada (opcional)
 app.get('/', (req, res) => {
