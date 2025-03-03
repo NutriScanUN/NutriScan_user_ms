edf345c3ecaf0aa0b46545ff66f6db670e8c4267
Juan Carreño <jcarrenoar@unal.edu.co>
2025-03-03 10:52:07 -0500

    update prometheus

--- src/app.js
+++ src/app.js
@@ -11,8 +11,38 @@ const promClient = require("prom-client");
 // Crear una instancia de la aplicación Express
 const app = express();
 
+const register = promClient.register;
 const collectDefaultMetrics = promClient.collectDefaultMetrics;
-collectDefaultMetrics();
+collectDefaultMetrics({ register });
+
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duración de las solicitudes HTTP en segundos",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.01, 0.05, 0.1, 0.5, 1, 2, 5] // Intervalos de tiempo en segundos
+});
+register.registerMetric(httpRequestDurationMicroseconds );
+
+const cacheRequests = new promClient.Counter({
+  name: "user_api_cache_total",
+  help: "Total de requests al cache",
+  labelNames: ["instance", "type"]
+});
+
+register.registerMetric(cacheRequests);
+
+const httpRequestTotal = new promClient.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP recibidas',
+  labelNames: ['method', 'route', 'status_code'],
+});
+
+// Simulación: Incrementar contador cuando se consulta caché
+app.get("/cache", (req, res) => {
+  cacheRequests.inc({ instance: "user-api", type: "Request" });
+  res.json({ message: "Cache request counted" });
+});
+
 
 // Configurar el middleware para parsear el cuerpo de las solicitudes (JSON)
 app.use(bodyParser.json());
@@ -26,7 +56,7 @@ const options = {
         },
         servers:[
             {
-                url: `https://34.2.5.32:${process.env.PORT}/api`,
+                url: `http://localhost:${process.env.PORT}/api`,
             }
         ]
     },
@@ -34,14 +64,28 @@ const options = {
 }
 
 const swaggerSpec = SwaggerJSDoc(options);
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on("finish", () => {
+    httpRequestTotal.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status_code: res.statusCode,
+    });
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
 app.use('/docs', SwaggerUI.serve,SwaggerUI.setup(swaggerSpec))
 
 // Rutas de la API
 app.use('/api', apiRoutes);  // Rutas para interactuar con el otro microservicio
 
 app.get("/metrics", async (req, res) => {
-    res.set("Content-Type", promClient.register.contentType);
-    res.end(await promClient.register.metrics());
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics());
   });
 
 // Ruta de bienvenida o predeterminada (opcional)
